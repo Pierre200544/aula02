a4bc1d0523590fb1ab875f85dfd1d452ecf7fae6
Pierre200544 <tuituifodaz@gmail.com>
2025-03-27 09:42:08 -0300

    Add files via upload

---
+++ Aula03_Lucas/script.js
@@ -0,0 +1,7 @@
+alert("Seja Bem-Vindo ao Portal")
+let nome = prompt("Digite o seu nome: ")
+let idade = parseInt(prompt("Digite a sua idade: "))
+alert("Seu usuario será: ", nome + idade)
+let usuario = nome + idade
+let senha = parseInt(prompt("Digite a sua senha: "))
+alert("Você foi cadastrado")
